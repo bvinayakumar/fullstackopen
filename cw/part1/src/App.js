586d9d733249388682d414cab05d6306bb40a592
Vinaya Kumar B <vinaya.bendi@man-es.com>
2021-07-01 18:56:10 +0530

    Part 1(c) course work

--- cw/part1/src/App.js
+++ cw/part1/src/App.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Hello = (props) => {
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
     <div>
-      <p>Hello world {props.name}, you are {props.age} years old</p>
+      <p>Hello world {name}, you are {age} years old</p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
 const Footer = () => {
   return (
@@ -19,12 +30,30 @@ const Footer = () => {
 const App = () => {
   const name = 'Peter'
   const age = 10
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <>
       <h1>Greetings</h1>
       <Hello name="Maya" age={26 + 10} />
       <Hello name={name} age={age} />
+      <Display counter={counter}/>
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />
       <Footer />
     </>
   )
