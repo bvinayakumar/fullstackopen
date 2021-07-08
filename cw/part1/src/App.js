fbf3b01c5a5debc58957b2e170ffc66ed25c4519
Vinaya Kumar B <vinaya.bendi@man-es.com>
2021-07-08 18:14:19 +0530

    Part 1(d) course work

--- cw/part1/src/App.js
+++ cw/part1/src/App.js
@@ -5,7 +5,9 @@ const Hello = ({ name, age }) => {
 
   return (
     <div>
-      <p>Hello world {name}, you are {age} years old</p>
+      <p>
+        Hello world {name}, you are {age} years old
+      </p>
       <p>So you were probably born in {bornYear()}</p>
     </div>
   )
@@ -14,15 +16,29 @@ const Hello = ({ name, age }) => {
 const Display = ({ counter }) => <div>{counter}</div>
 
 const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
+  <button onClick={handleClick}>{text}</button>
 )
 
 const Footer = () => {
   return (
     <div>
-      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
+      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+    </div>
+  )
+}
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
 }
@@ -30,7 +46,21 @@ const Footer = () => {
 const App = () => {
   const name = 'Peter'
   const age = 10
-  const [ counter, setCounter ] = useState(0)
+  const [counter, setCounter] = useState(0)
+
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
   const increaseByOne = () => setCounter(counter + 1)
   const decreaseByOne = () => setCounter(counter - 1)
@@ -39,21 +69,17 @@ const App = () => {
   return (
     <>
       <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
+      <Hello name='Maya' age={26 + 10} />
       <Hello name={name} age={age} />
-      <Display counter={counter}/>
-      <Button
-        handleClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        handleClick={setToZero}
-        text='zero'
-      />     
-      <Button
-        handleClick={decreaseByOne}
-        text='minus'
-      />
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text='plus' />
+      <Button handleClick={setToZero} text='zero' />
+      <Button handleClick={decreaseByOne} text='minus' />
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
       <Footer />
     </>
   )
